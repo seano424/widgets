3d5f2fc3f7a0c19b23bd9719803744c8c0f48f56
Sean OReilly <soreilly424@gmail.com>
2021-01-28 17:43:30 -0500

    usestate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordion from './components/Accordion'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    items: [
+      { title: "What is a dog?", content: 'A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.'},
+      { title: 'What kinds of dogs are there?', content: 'There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.'},
+      { title: 'How do you aquire a dog?', content: 'Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.'}
+    ]
+  }
+
+  render () {
+    return (
+      <div>
+        <Accordion items={this.state.items}/>
+      </div>
+    );  
+  }
 }
 
 export default App;
---
+++ src/components/Accordion.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const Accordion = ({items}) => {
+
+  const [ activeIndex, setActiveIndex ] = useState(null);
+
+  const onTitleClick = (index) => {
+    console.log('clicked ' + index);
+    setActiveIndex(index);
+  }
+
+  
+  const item = items.map((item, index) => {
+    const active = activeIndex === index ? 'active' : ''
+    return (
+      <React.Fragment key={item.title}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    )
+  })
+
+  return (
+    <div className="ui styled accordion">
+      {item}
+    </div>
+  )
+}
+
+export default Accordion;
